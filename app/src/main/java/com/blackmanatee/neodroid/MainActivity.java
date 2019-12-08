68c8a3da5722339593d53a3753a5dbd0f9274d3b
DeCorben <decorben@gmail.com>
2019-12-08 11:07:15 -0500

    Correct layout

--- app/src/main/java/com/blackmanatee/neodroid/MainActivity.java
+++ app/src/main/java/com/blackmanatee/neodroid/MainActivity.java
@@ -1,14 +1,14 @@
-package com.blackmanatee.testbed;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.blackmanatee.neodroid;
 
 import android.os.Bundle;
+import android.app.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.neodrive);
+		setTheme(android.R.style.Theme_DeviceDefault);
     }
 }
